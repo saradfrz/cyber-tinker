636529783a107cda714518d21d843371d467b774
Sara Fernandez <saradfrz@gmail.com>
2024-03-10 06:45:33 -0300

    Update forex_data_pipeline.py

--- dags/forex_data_pipeline.py
+++ dags/forex_data_pipeline.py
@@ -1,34 +1,52 @@
 from airflow import DAG
-from airflow.providers.http.sensors.http import HttpSensor
+from airflow.sensors.http_sensor import HttpSensor
 from airflow.sensors.filesystem import FileSensor
+from airflow.operators.python_operator import PythonOperator
+from airflow.operators.bash_operator import BashOperator
 from datetime import datetime, timedelta
 
+import csv
+import requests
+import json
+
 default_args = {
-    "owner": "airflow",
-    "email_on_failure": False,
-    "email_on_retry": False,
-    "email": "admin@localhost.com",
-    "refresh": 1,
-    "retry_delay": timedelta(minutes=5)
-}
+            "owner": "airflow",
+            "start_date": datetime(2019, 1, 1),
+            "email_on_failure": False,
+            "email_on_retry": False,
+            "email": "youremail@host.com",
+            "retries": 1,
+            "retry_delay": timedelta(minutes=5)
+        }
 
-def process_forex_rates():
-    # This function can be used to process the forex rates once they are available
-    # You can add your processing logic here
-    pass
+# Download forex rates according to the currencies we want to watch
+# described in the file forex_currencies.csv
+def download_rates():
+    BASE_URL = "https://gist.githubusercontent.com/marclamberti/f45f872dea4dfd3eaa015a4a1af4b39b/raw/"
+    ENDPOINTS = {
+        'USD': 'api_forex_exchange_usd.json',
+        'EUR': 'api_forex_exchange_eur.json'
+    }
+    with open('/opt/airflow/dags/files/forex_currencies.csv') as forex_currencies:
+        reader = csv.DictReader(forex_currencies, delimiter=';')
+        for idx, row in enumerate(reader):
+            base = row['base']
+            with_pairs = row['with_pairs'].split(' ')
+            indata = requests.get(f"{BASE_URL}{ENDPOINTS[base]}").json()
+            outdata = {'base': base, 'rates': {}, 'last_update': indata['date']}
+            for pair in with_pairs:
+                outdata['rates'][pair] = indata['rates'][pair]
+            with open('/opt/airflow/dags/files/forex_rates.json', 'a') as outfile:
+                json.dump(outdata, outfile)
+                outfile.write('\n')
 
-def process_forex_currencies_file():
-    # This function can be used to process the forex currencies file once it is available
-    # You can add your processing logic here
-    pass
-
-with DAG("forex_data_pipeline", start_date=datetime(2024, 3, 1),
-         schedule_interval="@daily", default_args=default_args, catchup=False) as dag:
-
+with DAG(dag_id="forex_data_pipeline_v_5", schedule_interval="@daily", default_args=default_args, catchup=False) as dag:
+    
     is_forex_rates_available = HttpSensor(
         task_id="is_forex_rates_available",
+        method="GET",
         http_conn_id="forex_api",
-        endpoint="marclamberti/f45f872dea4dfd3eaa015a4a1af4b39b",
+        endpoint="latest",
         response_check=lambda response: "rates" in response.text,
         poke_interval=5,
         timeout=20
@@ -42,8 +60,18 @@ with DAG("forex_data_pipeline", start_date=datetime(2024, 3, 1),
         timeout=20
     )
 
+    downloading_rates = PythonOperator(
+            task_id="downloading_rates",
+            python_callable=download_rates
+    )
 
-
-    # Define task dependencies
-    is_forex_rates_available >> is_forex_currencies_file_available
-
+    saving_rates = BashOperator(
+        task_id="saving_rates",
+        bash_command="""
+            hdfs dfs -mkdir -p /forex && \
+            hdfs dfs -put -f $AIRFLOW_HOME/dags/files/forex_rates.json /forex
+            """
+    )
+         
+        
+    is_forex_rates_available >> is_forex_currencies_file_available >> downloading_rates >> saving_rates
